b65635545b0355b966abc8e9e67950d70fdd185f
keeb <keeb@keeb.dev>
2023-02-05 16:32:43 -0800

    finish chapter8

--- ch8.lisp
+++ ch8.lisp
@@ -19,6 +19,48 @@
 ;; we want to make this work, 
 ; i.e, give me all primes between 0 and 19
 
+(defmacro do-primes (var-and-range &rest body)
+    (let ((var (first var-and-range))
+        (start (second var-and-range))
+        (end (third var-and-range)))
+        `(do ((, var (next-prime ,start) (next-prime (1+, var))))
+            ((> ,var ,end))
+            ,@body)))
+
 (do-primes p 0 19)
     (format t "~d " p)
+
+;; doing this with (do)
+
+(do ((p (next-prime 0) (next-prime (1+ p))))
+    ((> p 19))
+    (format t "~d " p))
+
+;; a shorter version of (do-primes) macro
+
+(defmacro do-primes ((var start end) &body body)
+    `(do ((,var (next-prime ,start) (next-prime (1+ ,var)))
+        (ending-value ,end))
+        ((> ,var ending-value))
+        ,@body))
+
+;; gensym can be used to generate unique symbols in macros so that there's no collision between the name of the 
+;; parameter/variable and the name of the variable in the body of the macro
+
+; here's a gensym version of the do-primes macro
+(defmacro do-primes ((var start end) &body body)
+    (let ((ending-value (gensym)))
+        `(do ((,var (next-prime ,start) (next-prime (1+ ,var)))
+            (,ending-value ,end))
+            ((> ,var ,ending-value))
+            ,@body)))
+
+;; Break 15 [17]> (macroexpand-1 '(do-primes (ending-value 0 (random 100))))
+; (DO ((ENDING-VALUE (NEXT-PRIME 0) (NEXT-PRIME (1+ ENDING-VALUE))) (#:G3141 (RANDOM 100))) ((> ENDING-VALUE #:G3141))) ;
+; T
+
+; the real form of creating a macro if possibles takes the following form...
+; (defmacro name ((var start end) &body body)
+;     (let ((some-var (gensym))) 
+;         `(some-form ,var ,start ,end ,some-var ,@body)))
 
